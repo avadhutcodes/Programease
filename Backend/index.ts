e0e0f93a5c49d018f42e70723bdf48bcac18946b
Avadhut Shinde <avadhutrendergithub@gmail.com>
2026-08-31 15:35:37 +0530

    made small wording changes

--- Backend/index.ts
+++ Backend/index.ts
@@ -14,12 +14,12 @@ app.use(express.json());
 connectDB();
 
 app.post("/submission", (req,res) => {
-    const Code = req.body.code;
+    const code = req.body.code;
     const language = req.body.language;
 
     /* sending the user's code to the worker via redis queue -- 1st job of backend done*/
 
-    client.lPush("problems",JSON.stringify({Code,language}));
+    client.lPush("problems",JSON.stringify({code,language}));
 
 
 
